482c7f58940279c7d340b3924531aaa276aeddb5
sieris <sierisalmeida@gmail.com>
2025-04-04 20:51:00 -0300

    commit

---
+++ src/app/index.tsx
@@ -0,0 +1,9 @@
+import { View, Text } from "react-native";
+
+export default function Index (){ //exportanto por padrao a funcao index
+    return ( //ele entrega tudo que esta aqui dentro(uso externo)
+        <View>
+            <Text>eU SOU o meelhor homem arannnnhaa</Text>
+        </View>
+    );
+}
