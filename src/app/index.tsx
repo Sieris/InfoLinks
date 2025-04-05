e021b12568b78c916ad71dfb95ef3d1bbe620d2e
sieris <sierisalmeida@gmail.com>
2025-04-05 15:22:20 -0300

    inseri novos elementos

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,9 +1,13 @@
 import { View, Text } from "react-native";
-
+// Export default é o metodo usado para dizer ao ExporRouter que esse componente é uma Rota
 export default function Index (){ //exportanto por padrao a funcao index
     return ( //ele entrega tudo que esta aqui dentro(uso externo)
-        <View>
-            <Text>eU SOU o meelhor homem arannnnhaa</Text>
+       //view é comparada a div
+       <View> 
+            <Text>eU SOU o meelhor homem arannnnhaa</Text> 
+            <Text>cru cru cru</Text>
+            <Text> cri cri cri</Text>
+          
         </View>
     );
 }
